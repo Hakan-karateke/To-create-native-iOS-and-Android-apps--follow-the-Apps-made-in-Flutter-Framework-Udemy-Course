09891bc8b92d64eaa6847982afd5720cba89c805
Hakan <hakanali231@gmail.com>
2024-08-07 11:28:01 +0300

    meals app completed

--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals/models/category.dart';
 
-class CategoryGridItem extends StatelessWidget{
-
+class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
-    required this.onSelectCategory,
-    required this.category,
     super.key,
+    required this.category,
+    required this.onSelectCategory,
   });
 
   final Category category;
@@ -14,7 +14,6 @@ class CategoryGridItem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    
     return InkWell(
       onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
@@ -22,22 +21,20 @@ class CategoryGridItem extends StatelessWidget{
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-      
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
         child: Text(
           category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
         ),
       ),
     );
--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:meals/models/meal.dart';
 import 'package:transparent_image/transparent_image.dart';
+
+import 'package:meals/widgets/meal_item_trait.dart';
+import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,26 +34,29 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
               left: 0,
-              right: 50,              
+              right: 0,
               child: Container(
                 color: Colors.black54,
-                padding: const EdgeInsets.symmetric(
-                  vertical: 6,
-                  horizontal: 44,
-                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
                 child: Column(
                   children: [
                     Text(
@@ -47,7 +64,7 @@ class MealItem extends StatelessWidget {
                       maxLines: 2,
                       textAlign: TextAlign.center,
                       softWrap: true,
-                      overflow: TextOverflow.ellipsis,
+                      overflow: TextOverflow.ellipsis, // Very long text ...
                       style: const TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
@@ -56,14 +73,28 @@ class MealItem extends StatelessWidget {
                     ),
                     const SizedBox(height: 12),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        )
                       ],
                     ),
                   ],
                 ),
-              ), 
-            ),
+              ),
+            )
           ],
         ),
       ),
