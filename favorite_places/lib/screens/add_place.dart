39fae594aa4f8cae750c2ea4ea800fc305ebab15
Hakan <hakanali231@gmail.com>
2024-08-15 09:53:26 +0300

    favorite placess app finished

--- favorite_places/lib/screens/add_place.dart
+++ favorite_places/lib/screens/add_place.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
 import 'package:favorite_places/widgets/location_input.dart';
@@ -19,22 +20,31 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
+      return;
+    }
+
+    ref
+        .read(userPlacesProvidier.notifier)
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController;
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    void _savePlace() {
-      final _enteredTitle = _titleController.text;
-
-      if (_enteredTitle.isEmpty || _selectedImage == null) {
-        return;
-      }
-
-      ref
-          .read(userPlacesProvidier.notifier)
-          .addPlace(_enteredTitle, _selectedImage!);
-      Navigator.of(context).pop();
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add New Place'),
@@ -63,7 +73,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             const SizedBox(
               height: 10,
             ),
-            LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -1,17 +1,14 @@
 import 'dart:convert';
 
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/map.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
-
-  const LocationInput({
-    super.key,
-    required this.onSelectLocation
-  })
-
+  const LocationInput({super.key, required this.onSelectLocation});
 
   final void Function(PlaceLocation location) onSelectLocation;
   @override
@@ -24,13 +21,32 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
-  String get LocationImage {
+  String get locationImage {
     if (_pickedLocation == null) {
       return '';
     }
     final lat = _pickedLocation!.latitude;
     final lng = _pickedLocation!.longitude;
     return 'https://maps.googleapis.com/maps/api/staticmap?center$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=YOUR_API_KEY';
+  }
+
+  Future<void> _savePlace(double latitude, double longtitude) async {
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longtitude&key=YOUR_API_KEY');
+    final response = await http.get(url);
+    final resdata = json.decode(response.body);
+    final address = resdata['results'][0]['formatted_address'];
+
+    setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: latitude,
+        longitude: longtitude,
+        address: address,
+      );
+      _isGettingLocation = false;
+    });
+
+    widget.onSelectLocation(_pickedLocation!);
   }
 
   void _getCurrentLocation() async {
@@ -68,22 +84,21 @@ class _LocationInputState extends State<LocationInput> {
       return;
     }
 
-    final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=YOUR_API_KEY');
-    final response = await http.get(url);
-    final resdata = json.decode(response.body);
-    final address = resdata['results'][0]['formatted_address'];
+    _savePlace(lat, lng);
+  }
 
-    setState(() {
-      _pickedLocation = PlaceLocation(
-        latitude: lat!,
-        longitude: lng!,
-        address: address,
-      );
-      _isGettingLocation = false;
-    });
+  void _selectOnMap() async {
+    final pickedLocation = await Navigator.of(context).push<LatLng>(
+      MaterialPageRoute(
+        builder: (ctx) => const MapScreen(),
+      ),
+    );
 
-    widget.onSelectLocation();
+    if (pickedLocation == null) {
+      return;
+    }
+
+    _savePlace(pickedLocation.latitude, pickedLocation.longitude);
   }
 
   @override
@@ -98,7 +113,7 @@ class _LocationInputState extends State<LocationInput> {
 
     if (_pickedLocation != null) {
       previewContent = Image.network(
-        LocationImage,
+        locationImage,
         fit: BoxFit.cover,
         width: double.infinity,
         height: double.infinity,
@@ -133,7 +148,9 @@ class _LocationInputState extends State<LocationInput> {
             TextButton.icon(
               icon: const Icon(Icons.map),
               label: const Text('Select on map'),
-              onPressed: () {},
+              onPressed: () {
+                _selectOnMap();
+              },
             ),
           ],
         )
